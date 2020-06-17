8124283cde459bcf2b0ac6aa9910616f3c7a0717
James <erresen@users.noreply.github.com>
2020-06-17 13:18:14 +0100

    added gh-pages deployment

---
+++ deploy.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env sh
+
+# abort on errors
+set -e
+
+npm run build
+
+cd dist
+
+git init
+git add -A
+git commit -m 'deploy'
+
+git push -f git@github.com:erresen/throwaway.git master:gh-pages
+
+cd -

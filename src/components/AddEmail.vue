487f8f9916b4cb6427f6613b6b0defb2fd5e6f48
James <erresen@users.noreply.github.com>
2020-06-18 18:09:02 +0100

    basic layout styling

--- src/components/AddEmail.vue
+++ src/components/AddEmail.vue
@@ -1,18 +1,36 @@
 <template>
   <div>
-    <form>
-      <input type="radio" value="mailinator" v-model="service" />
-      <input type="radio" value="maildrop" v-model="service" />
-      <input type="text" name="email" placeholder="Email" v-model="email" />
-      <input type="button" value="Copy" class="btn" />
-      <input type="button" value="Inbox" class="btn" />
+    <div class="current-container">
+      <div class="services-container">
+        <label for="mailinator-radio">Mailinator</label>
+        <input
+          type="radio"
+          id="mailinator-radio"
+          value="mailinator"
+          v-model="service"
+        />
+        <label for="maildrop-radio">Maildrop</label>
+        <input
+          type="radio"
+          id="maildrop-radio"
+          value="maildrop"
+          v-model="service"
+        />
+      </div>
+      <div class="email-container">
+        <input type="text" name="email" placeholder="Email" v-model="email" />
+        <input type="button" value="Copy" class="btn" />
+        <input type="button" value="Inbox" class="btn" />
+      </div>
+    </div>
+    <div class="generate-new-container">
       <input
         type="button"
         value="Generate"
         class="btn"
         v-on:click="generateNew"
       />
-    </form>
+    </div>
   </div>
 </template>
 
@@ -57,16 +75,22 @@ export default {
 </script>
 
 <style scoped>
-form {
-  display: flex;
+.current-container {
+  display: grid;
+  justify-items: center;
+  width: 100%;
+}
+.email-container {
+  display: grid;
+  grid-template-columns: auto 150px 150px;
+  max-width: 1000px;
+  width: 100%;
+  margin: 20px;
 }
 
 input[type="text"] {
-  flex: 10;
+  text-align: center;
+  font-size: 1.5em;
   padding: 5px;
-}
-
-input [type="submit"] {
-  flex: 2;
 }
 </style>

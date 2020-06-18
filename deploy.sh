0f8928f4fec5ea34dbdbca049a185203cbc53854
James <erresen@users.noreply.github.com>
2020-06-18 15:26:06 +0100

    changed deploy script to use ssh instead of https

--- deploy.sh
+++ deploy.sh
@@ -11,6 +11,6 @@ git init
 git add -A
 git commit -m 'deploy'
 
-git push -f https://github.com/erresen/throwaway.git master:gh-pages
+git push -f git@github.com:erresen/throwaway.git master:gh-pages
 
 cd -

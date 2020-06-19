3574f9cafc572fbb3ca43499dcc65d2ae8ed2e3b
James <erresen@users.noreply.github.com>
2020-06-19 21:10:09 +0100

    make windows look less shite with a webfont

--- src/App.vue
+++ src/App.vue
@@ -41,7 +41,7 @@ export default {
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: Avenir, Poppins, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
